d7054d8c8aac5d9b5fd3661bffec8559b2c8b988
Bearukun <bearukun@gmail.com>
2017-05-16 14:13:41 +0200

    Updated to use broken english in API

--- airline/src/main/java/entity/Flight.java
+++ airline/src/main/java/entity/Flight.java
@@ -7,7 +7,7 @@ public class Flight {
     public String date; //Format: YEAR-MONTH-DATE(T)HOUR - Date+time in ISO-8601 format. Eg. "2017-05-30-T13:00:00.000Z"
     public int numberOfSeats;
     public Number totalPrice; //In euro
-    public int travelTime; //In minutes
+    public int traveltime; //In minutes
     public String origin; //IATA-Code eg. "CPH"
     public String destination; //IATA-Code eg. "JFK"
 
