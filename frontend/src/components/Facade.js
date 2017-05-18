4fa5a84f379e5414fcbb044056da162cd6054af8
DjKirkovic <kirkovic@gmail.com>
2017-05-18 12:41:20 +0200

    Facade post method expanded to talk to backend

--- frontend/src/components/Facade.js
+++ frontend/src/components/Facade.js
@@ -1,6 +1,7 @@
 import { observable, action, computed} from "mobx";
 import fetchHelper from "./fetchHelpers"
 const URL = "http://localhost:8084/seedMaven/api/flights/";
+const URL2 = "http://localhost:8084/seedMaven/api/flights/";
 const abc = "CPH/2017-05-04/4";
 class Facade {
     @observable messageFromServer = "";
@@ -19,6 +20,25 @@ class Facade {
                 "origin": "",
                 "destination": ""
             }]
+    }];
+    @observable _booking = [{
+        "flightNumber": "COL2256",
+        "origin": "Copenhagen Kastrup(CPH)",
+        "destination": "Charles de Gaulle International(CDG)",
+        "date": "2016-04-06T10:00:00.000Z",
+        "flightTime": 120,
+        "numberOfSeats": 2,
+        "reserveeName": "Peter Hansen",
+        "passengers": [
+            {
+                "firstName": "Peter",
+                "lastName": "Peterson"
+            },
+            {
+                "firstName": "Jane",
+                "lastName": "Peterson"
+            }
+        ]
     }];
 
     @action
@@ -64,43 +84,55 @@ class Facade {
     @action
     submitBooking = (booking) => {
         // console.log(booking);
-        alert("Booking details: "+booking);
-        // this.errorMessage = "";
-        // this.messageFromServer = "";
-        // let errorCode = 200;
-        // const options = fetchHelper.makeOptions("POST", true);
-        // var conf = {
-        //     method: 'post',
-        //     headers: {
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         title: book.title,
-        //         info: book.info,
-        //         moreInfo: book.moreInfo
-        //     })
-        // };
+        // alert("Booking details: "+booking);
+        this.errorMessage = "";
+        this.messageFromServer = "";
+        let errorCode = 200;
+        const options = fetchHelper.makeOptions("POST", true);
+        var conf = {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                flightID: "CA123",
+                numberOfSeats: 2,
+                "reserveeName": "Peter Hansen",
+                "reservePhone":"12345678",
+                "reserveeEmail": "peter@peter.dk",
+                "passengers":[
+                    {
+                        "firstName": "Peter",
+                        "lastName": "Peterson"
+                    },
+                    {
+                        "firstName": "Jane",
+                        "lastName": "Peterson"
+                    }
+                ]
+            })
+        };
         // fetch(URL + "api/user/add", conf, options)
-        //     .then((res) => {
-        //         if (res.status > 210 || !res.ok) {
-        //             errorCode = res.status;
-        //         }
-        //         return res.json();
-        //     })
-        //     .then(action((res) => {  //Note the action wrapper to allow for useStrict
-        //         if (errorCode !== 200) {
-        //             throw new Error(`${res.error.message} (${res.error.code})`);
-        //         }
-        //         else {
-        //             this._books.replace(res);
-        //             const addedBookTitle = res.title;
-        //             return addedBookTitle;
-        //         }
-        //     })).catch(err => {
-        //     //This is the only way (I have found) to verify server is not running
-        //     this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
-        // })
+        fetch(URL2, conf, options)
+            .then((res) => {
+                if (res.status > 210 || !res.ok) {
+                    errorCode = res.status;
+                }
+                return res.json();
+            })
+            .then(action((res) => {  //Note the action wrapper to allow for useStrict
+                if (errorCode !== 200) {
+                    throw new Error(`${res.error.message} (${res.error.code})`);
+                }
+                else {
+                    alert(res);
+                    // this._booking.replace(res);
+                }
+            })).catch(err => {
+            //This is the only way (I have found) to verify server is not running
+            this.setErrorMessage(fetchHelper.addJustErrorMessage(err));
+        })
     }
 
     // @action
